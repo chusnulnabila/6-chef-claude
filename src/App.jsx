3c9780131741e8f39d071c9013f1c18d1ff314a6
chusnul nabila <chusnulnab@gmail.com>
2024-12-10 22:23:05 +0700

    not finished yet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./component/Header";
+import Mainn from "./Mainn.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Mainn />
+    </>
+  );
+}
